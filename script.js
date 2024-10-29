a111c1b9a7b71112cd772f94ed5cc5c44cd5ee98
Hetal Nathvani <nathvanihetal@gmail.com>
2024-10-29 14:58:44 +0530

    UI changes | WIP

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.getElementById("search-btn");
+  const usernameInput = document.getElementById("user-input");
+});
